88cf23c3ed87375ca4d989a7e418a9600726b8cf
zensan <sanjos100@gmail.com>
2016-03-28 16:26:21 +0530

    Add Async Capnproto test

---
+++ async_capnp/CapnpServer.cpp
@@ -0,0 +1,48 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <system_error>
+
+#include <capnp/rpc-twoparty.h>
+#include <kj/async-io.h>
+#include <kj/async.h>
+
+#include "bench.capnproto.h"
+
+#include <glog/logging.h>
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+class SessionImpl : public benchmark::Database::Server
+{
+  public:
+  SessionImpl()
+  {
+  }
+
+  virtual ::kj::Promise<void> get(GetContext context) override
+  {
+    context.getResults().setValue(context.getParams().getKey());
+    return kj::READY_NOW;
+  }
+  
+};
+
+int main(int argc, char* argv[])
+{
+  std::string addr_ = "localhost";
+  int port_ = 8888;
+
+  auto ioContext = kj::setupAsyncIo();
+
+  capnp::TwoPartyServer server(kj::heap<SessionImpl>());
+
+  auto address = ioContext.provider->getNetwork()
+    .parseAddress(addr_, port_).wait(ioContext.waitScope);
+  
+  auto listener = address->listen();
+  auto listenPromise = server.listen(*listener);
+
+  listenPromise.wait(ioContext.waitScope);
+}
